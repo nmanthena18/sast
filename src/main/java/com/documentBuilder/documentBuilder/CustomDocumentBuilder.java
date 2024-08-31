b5f0ba204212a3ea9dbb7834a286e4dbb0ff2117
nmanthena18 <nmanthena.it@gmail.com>
2024-08-31 11:47:09 +0530

    new changes

--- src/main/java/com/documentBuilder/documentBuilder/CustomDocumentBuilder.java
+++ src/main/java/com/documentBuilder/documentBuilder/CustomDocumentBuilder.java
@@ -2,6 +2,8 @@ package com.documentBuilder.documentBuilder;
 
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+
+import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.OutputKeys;
@@ -17,8 +19,11 @@ public class CustomDocumentBuilder { // Renamed the class to avoid conflict
         TransformerFactory tf = TransformerFactory.newInstance();
         Transformer transformer;
         try {
+            tf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+            tf.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "");
+            tf.setAttribute(XMLConstants.ACCESS_EXTERNAL_STYLESHEET, "");
             transformer = tf.newTransformer();
-            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
+            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
             transformer.setOutputProperty(OutputKeys.METHOD, "xml");
             transformer.setOutputProperty(OutputKeys.INDENT, "yes");
             transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
