11ca3762909a4c9956bf9c2e64b2e59af66c0b2c
nmanthena18 <nmanthena.it@gmail.com>
2024-08-31 11:26:51 +0530

    first commit

---
+++ src/main/java/com/documentBuilder/documentBuilder/CustomDocumentBuilder.java
@@ -0,0 +1,31 @@
+package com.documentBuilder.documentBuilder;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+public class CustomDocumentBuilder { // Renamed the class to avoid conflict
+    
+    public CustomDocumentBuilder() throws IOException {
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer transformer;
+        try {
+            transformer = tf.newTransformer();
+            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
+            transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+
+        } catch (TransformerException e) {
+            throw new IOException("Could not print Document or Node.", e);
+        }
+    }
+}
---
+++ src/main/java/com/documentBuilder/documentBuilder/DocumentBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.documentBuilder.documentBuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentBuilderApplication.class, args);
+	}
+
+}
